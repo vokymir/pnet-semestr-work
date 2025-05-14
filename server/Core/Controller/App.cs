4b0ca388e786ccecf84ea3638e9ec08902fbae05
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-05-14 12:40:07 +0200

    other things

--- server/Core/Controller/App.cs
+++ server/Core/Controller/App.cs
@@ -1,11 +1,28 @@
 namespace BirdWatching;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+using Microsoft.Data.Sqlite;
 
 public class App
 {
     public ICollection<Bird> Birds { get; set; } = new List<Bird>();
+
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello birdwatcher!");
+
+        var connection = new SqliteConnectionStringBuilder()
+        {
+            DataSource = "db.db"
+        };
+
+        var options = new DbContextOptionsBuilder<StagDbContext>().UseSqlite(connection.ConnectionString).Options;
+
+        var context = new StagDbContext(options);
+        context.Database.EnsureCreated();
+
+        var repo = new EFStudentRepository(context);
+        var repo = new EFStudentRepository(context);
     }
 }
 
