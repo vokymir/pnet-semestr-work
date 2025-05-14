6ecc6ccb17faab6e9350eaa90dcf9a9d317c366b
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-05-14 15:01:02 +0200

    update clients namespace

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
-namespace Client;
+namespace BirdWatching.Client;
 
 public class Program
 {
