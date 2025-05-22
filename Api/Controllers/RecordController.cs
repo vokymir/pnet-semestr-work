010d903ffa8feae1663647840f15b0e23eb81cee
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-05-22 14:16:07 +0200

    working on record controller

--- Api/Controllers/RecordController.cs
+++ Api/Controllers/RecordController.cs
@@ -1,6 +1,7 @@
 namespace BirdWatching.Api.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using BirdWatching.Shared.Model;
 
 /// <summary>
 /// Can do:
@@ -21,4 +22,41 @@ public class RecordController : BaseApiController
         Init();
     }
 
+    [HttpPost("Create/{token}")]
+    public IResult Create(string token, RecordDto recordDto)
+    {
+        var response = AuthUserByToken(token);
+        if (!response.Result.Equals(Results.Ok())) return response.Result;
+
+        Record r = recordDto.ToEntity();
+
+        var w = _watcherRepo.GetById(r.WatcherId);
+        if (w is null) return Results.NotFound("Watcher not found.");
+        if (!w.Curators.Contains(response.User!)) return Results.Problem("Don't have permission to edit watcher.");
+
+        try
+        {
+            _recordRepo.Add(r);
+        }
+        catch (Exception e)
+        {
+            return Results.Problem(e.Message);
+        }
+
+        return Results.Ok();
+    }
+
+    [HttpGet("Get")]
+    public IResult Get(int recordId)
+    {
+        var r = _recordRepo.GetById(recordId);
+        if (r is null) return Results.NotFound();
+        else return Results.Ok(r.ToFullDto());
+    }
+
+    [HttpGet("GetWatchers")]
+    public IResult GetWatchers(int watcherId)
+    {
+        throw new NotImplementedException();
+    }
 }
