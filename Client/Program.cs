b2ddd0ea1e60bc9c1fc1caa29c91081d88dd211f
Vokymir <22869857+vokymir@users.noreply.github.com>
2025-06-25 14:30:51 +0200

    C: working basic layout

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,6 +13,8 @@ public class Program
 
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+        builder.Services.AddBlazorBootstrap();
+
         await builder.Build().RunAsync();
     }
 }
